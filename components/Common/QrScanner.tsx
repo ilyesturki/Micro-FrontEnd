345e5fc67af24f68a76dfeed19bc30aaedbf406c
ilyess.torki.v <ilyess.torki.v@gmail.com>
2026-04-29 21:52:03 +0000

    fix: patch Next.js and React CVE vulnerabilities

--- components/Common/QrScanner.tsx
+++ components/Common/QrScanner.tsx
@@ -61,7 +61,7 @@ export default function QRScanner() {
 
             // ✅ 2. Send to backend
             const res = await fetch(
-              `${process.env.NEXT_PUBLIC_API_URL}/parking/door`,
+              `${process.env.NEXT_PUBLIC_API_BASE_URL}/parking/door`,
               {
                 method: "POST",
                 headers: {
